6706cfb5ea262dab9a785e124d144b2010f9b73c
GiullianoMartinez <giulliano.martinez.y@gmail.com>
2025-08-12 13:30:11 -0400

    Se añade nueva función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-a
+let amigos = [];
+
+function agregarAmigo() {
+    textoIngresado = document.getElementById("amigo").value;
+
+    if (textoIngresado) {
+        amigos.push(textoIngresado);
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+
+    let caja = document.querySelector("#amigo");
+    caja.value = "";
+    console.log(amigos);
+}
+
+
+
+
