999738e98d931509e04bcbff8aaca8aa02083efb
GiullianoMartinez <giulliano.martinez.y@gmail.com>
2025-08-12 13:45:47 -0400

    Se desarrolla nueva función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ function agregarAmigo() {
 
     if (textoIngresado) {
         amigos.push(textoIngresado);
+        actualizarAmigos();
     } else {
         alert("Por favor, inserte un nombre.");
     }
 
     let caja = document.querySelector("#amigo");
     caja.value = "";
-    console.log(amigos);
 }
 
 
+function actualizarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
 
+    let amigo;
+    for (amigo of amigos) {
+        lista.innerHTML += "<li>" + amigo + "</li>";
+    }
+}
 
